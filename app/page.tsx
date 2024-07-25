a52d6a57b4a20e2729b0d3e74d4ca0e6a117227e
Michael Martinez <mmart196@fiu.edu>
2024-07-25 12:25:35 +0700

    added authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
+import { Authenticator } from '@aws-amplify/ui-react'
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
@@ -31,13 +32,23 @@ export default function App() {
     });
   }
 
+
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
   return (
+
+      <Authenticator>
+          {({ signOut, user }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -46,7 +57,10 @@ export default function App() {
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
+        <button onClick={signOut}>Sign out</button>
       </div>
     </main>
+          )}
+              </Authenticator>
   );
 }
